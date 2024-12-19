f2984bf06d434b245fcc7212ec752dbc65423740
Ryan Elliot <86525779+RyanMElliot@users.noreply.github.com>
2024-12-19 15:09:55 +0000

    Modify miller.py be constructed using functions

--- miller.py
+++ miller.py
@@ -1,18 +1,21 @@
-from numpy import *
-
-A=2.2
-kappa=1.5
-delta=0.3
-R0=2.5
-theta=linspace(0,2*pi)
-r=R0/A
-R_s=R0+r*cos(theta+(arcsin(delta)*sin(theta)))
-Z_s=kappa*r*sin(theta)
-
+import numpy as np
 import matplotlib.pyplot as plt
+def main():
+    def flux_surface(A = 2.2,kappa = 1.5,delta = 0.3,R0 = 2.5):
+        theta = np.linspace(0, 2 * np.pi)
+        r = R0 / A
+        R_s = R0 + r * np.cos(theta + (np.arcsin(delta) * np.sin(theta)))
+        Z_s = kappa * r * np.sin(theta)
+        return (R_s,Z_s)
 
-plt.plot(R_s, Z_s)
-plt.axis("equal")
-plt.xlabel("R [m]")
-plt.ylabel("Z [m]")
-plt.savefig("./miller.png")
+    def plot_surface(R_s,Z_s,savefig = True):
+        plt.plot(R_s, Z_s)
+        plt.axis("equal")
+        plt.xlabel("R [m]")
+        plt.ylabel("Z [m]")
+        if savefig:
+            plt.savefig("./miller.png")
+
+
+if __name__ == "__main__":
+    main()
